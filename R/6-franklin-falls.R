7e8ae86ae983f77971cf0839a7062ea456a2d57f
Neal Maker <nealmaker@gmail.com>
2022-03-07 22:04:20 -0500

    fix ff cashflows

--- R/6-franklin-falls.R
+++ R/6-franklin-falls.R
@@ -1,5 +1,5 @@
 # 7.3% discount rate (from John McGann)
-load("results073.rda")
+load("results-073.rda")
 
 params <- params_default
 params$endyr <- 120
@@ -9,19 +9,22 @@ params_f$drate <- .073
 cut_yrs <- results[[1]]$cutyr
 ff_bareland_sim_073 <- new_simulation(cut_yrs, results[[1]], params = params_f)
 
-# calculates cash flows for one plot
+# calculates cash flows for one plot 
+## SHOULD USE MORTADJ LV & STUMP_AC?############################################
 cash_flows <- function(trees, logs, params) {
   standing <- trees %>%
     dplyr::group_by(year) %>%
-    dplyr::summarize(standing = sum(lv * tpa_tree, na.rm = T)) %>%
-    dplyr::mutate(standing  = standing / (1 + params$drate) ^ year)
+    dplyr::summarize(standing = sum(lv_mortadj * tpa_tree, na.rm = T)) %>%
+    dplyr::mutate(year = year + 30,
+                  standing  = standing / (1 + params$drate) ^ year)
   harvest <- logs %>%
     dplyr::rename(year = cutyr) %>%
     dplyr::group_by(year) %>%
-    dplyr::summarize(harvest = sum(stump_ac, na.rm = T)) %>%
-    dplyr::mutate(harvest = harvest / (1 + params$drate) ^ year,
+    dplyr::summarize(harvest = sum(stump_ac_mortadj, na.rm = T)) %>%
+    dplyr::mutate(year = year + 30,
+                  harvest = harvest / (1 + params$drate) ^ year,
                   year = year + .1)
-  df <- dplyr::left_join(standing, harvest, by = "year")
+  df <- dplyr::full_join(standing, harvest, by = "year")
   
   df$harvest[is.na(df$harvest)] <- 0
   
